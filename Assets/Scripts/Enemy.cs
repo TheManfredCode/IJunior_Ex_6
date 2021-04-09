f125cfbed433af09db369de2ace58901410e86ed
TheManfredCode <41202546+TheManfredCode@users.noreply.github.com>
2021-04-09 22:42:52 +0300

    Fix 2

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,23 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Transform))]
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _lifeTime;
     [SerializeField] private float _speed;
 
     private float _runningTime = 0f;
-    private Transform _transform;
-
-    private void Start()
-    {
-        _transform = GetComponent<Transform>();
-    }
 
     private void Update()
     {
-        _transform.Translate(0, _speed * Time.deltaTime * -1, 0);
+        transform.Translate(0, _speed * Time.deltaTime * -1, 0);
 
         if (_runningTime >= _lifeTime)
             Destroy(gameObject);
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,20 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Transform))]
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Enemy _template;
-    
-    private Transform _transform;
+    [SerializeField] private float _spawnRate;
 
-    public void Spawn()
-    {
-        Instantiate(_template, _transform.position, Quaternion.identity);
-    }
+    private float _runningTime;
+    private int _spawnIteration = 0;
+    private Transform[] _spawnPoints;
 
     private void Start()
     {
-        _transform = GetComponent<Transform>();
+        _spawnPoints = new Transform[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+            _spawnPoints[i] = transform.GetChild(i);
+    }
+
+    private void Update()
+    {
+        if (_runningTime <= _spawnRate)
+        {
+            _runningTime += Time.deltaTime;
+        }
+        else
+        {
+            _runningTime = 0f;
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        Instantiate(_template, _spawnPoints[_spawnIteration].position, Quaternion.identity);
+        _spawnIteration++;
+
+        if (_spawnIteration == _spawnPoints.Length)
+            _spawnIteration = 0;
     }
 }
